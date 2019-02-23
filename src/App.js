26b6b32a8d54720fb3c7cf460d139066a3c8bf52
SaroshKhawar496 <sarosh.496@gmail.com>
2019-02-22 22:52:55 -0500

    Added form, title, weather component. Added OpenWeatherMap API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather'
+
+const API_KEY = "eefaf10c9102d3879e42754b766070d2"
+
 class App extends Component {
+  getWeather = async () =>{
+    const api_call = await fetch(`https://samples.openweathermap.org/data/2.5/weather
+    ?q=London,uk&appid=${API_KEY}&units=metric`);
+    
+  }
   render() {
     return (
       <div>
-        React Weather App!!
+        <Titles />
+        <Form />
+        <Weather />
       </div>
     );
   }
