09d8aecf31d57adb1a4f593790e8340c18eef724
SaroshKhawar496 <sarosh.496@gmail.com>
2019-02-22 23:40:43 -0500

    Added to Weather.js. Added state to app.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import './App.css';
 
 import Titles from './components/Titles';
 import Form from './components/Form';
-import Weather from './components/Weather'
+import Weather from './components/Weather';
 
-const API_KEY = "eefaf10c9102d3879e42754b766070d2"
+const API_KEY = "eefaf10c9102d3879e42754b766070d2";
 
 class App extends Component {
+
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
   getWeather = async (e) =>{
     // prevent the default page refresh on button click in form.js
     e.preventDefault();
@@ -15,17 +25,44 @@ class App extends Component {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
 
-    const api_call = await fetch(`https://samples.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const api_call = await fetch(`https://samples.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=metric`);
 
     const data = await api_call.json();
-    console.log(data);
+    
+    // preventing app breaking if city and country not given
+    if(city && country){
+      console.log(data);
+    
+      this.setState(
+        {
+          temperature: data.main.temp,
+          city: data.name,
+          country: data.sys.country,
+          humidity: data.main.humidity,
+          description: data.weather[0].description,
+          error: ""
+        }
+      );
+    }
+
+
   }
   render() {
     return (
       <div>
         <Titles />
+
         <Form getWeather={this.getWeather}/>
-        <Weather />
+
+        <Weather 
+        temperature={this.state.temperature}
+        city={this.state.city}
+        country={this.state.country}
+        humidity={this.state.humidity}
+        description={this.state.description}
+        error={this.state.error}
+        />
+
       </div>
     );
   }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,9 +2,27 @@ import React, { Component } from 'react';
 
 class Weather extends Component {
     render(){
+        // let show = undefined;
+        // if (this.props.city && this.props.country){
+        //     show = (
+        //         <div>
+        //         <p>Location: {this.props.city}, {this.props.country}</p>
+        //         <p>Temperature: {this.props.temperature}</p>
+        //         <p>Humidity: {this.props.humidity}</p>
+        //         <p>Conditions: {this.props.description}</p>
+        //         </div>
+        //     );
+        // }
         return(
             <div>
-                Weather Component
+
+                {/* {show} */}
+
+                {this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
+                {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
+                {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+                {this.props.description && <p>Conditions: {this.props.description}</p>}
+            
             </div>
         );
     }
