5d9960e01c921fbc0b538f0410a89a2b86ec37ae
SaroshKhawar496 <sarosh.496@gmail.com>
2019-02-22 23:11:46 -0500

    Added inputs in form.js. added getWeather fxn in app.js

--- src/App.js
+++ src/App.js
@@ -8,16 +8,23 @@ import Weather from './components/Weather'
 const API_KEY = "eefaf10c9102d3879e42754b766070d2"
 
 class App extends Component {
-  getWeather = async () =>{
-    const api_call = await fetch(`https://samples.openweathermap.org/data/2.5/weather
-    ?q=London,uk&appid=${API_KEY}&units=metric`);
-    
+  getWeather = async (e) =>{
+    // prevent the default page refresh on button click in form.js
+    e.preventDefault();
+    // e will come from form.js
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    const api_call = await fetch(`https://samples.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+
+    const data = await api_call.json();
+    console.log(data);
   }
   render() {
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather}/>
         <Weather />
       </div>
     );
